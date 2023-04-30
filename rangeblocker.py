49e02f98de38f1c4da4f04c48369fbd5a3bf3cbb
comradeblin <gombai.gergely@gmail.com>
2023-04-30 18:07:44 +0200

    implemented ip -> as func

--- rangeblocker.py
+++ rangeblocker.py
@@ -3,7 +3,14 @@ from os import system
 
 system("if hash whois &> /dev/null; then echo 'Whois is installed'; else echo 'Whois not installed'; exit 100; fi")
 
-asn = input(str(Fore.GREEN+"Enter ASN: "))
+ip = input(str(Fore.GREEN+"Enter IP: "))
+
+awkascomm = """awk '$1=="origin:" { print $2}'"""
+
+system(f"whois {ip} | {awkascomm} >> as.tmp")
+
+asf = open("as.tmp", 'r')
+asn = asf.read()
 
 
 
@@ -15,11 +22,14 @@ system(f"whois -h whois.radb.net -- '-i origin {asn}' | {grepcomm} >> ranges.lst
 rangesf = open("ranges.lst", 'r')
 ranges = rangesf.readlines()
 
+print(ranges)
 for range in ranges:
+    print("1")
     range = range.strip()
     system(f"iptables -A INPUT -s {range} -j DROP")
     print(Fore.RED+"Blocked " + range)
 
 system("rm ranges.lst")
+system("rm as.tmp")
 
 print(Fore.GREEN+"Done")
